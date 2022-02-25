8b8d39bf4883fe14f2ff1bf8296bb10684692d0e
angiegyi <angelagyi@hotmail.com>
2022-02-25 21:11:07 +1100

    not hard coded

--- mini-melbourne/src/Map/map.js
+++ mini-melbourne/src/Map/map.js
@@ -12,6 +12,15 @@ export default function Map() {
   const [lat, setLat] = useState(-37.8183051340585);
   const [zoom, setZoom] = useState(15);
 
+  const coordinates = [
+    [[144.966964346166, -37.8183051340585],
+    [144.967731536194, -37.8180840725787],
+    [144.967915434574, -37.8180061406911],
+    [144.968831710922, -37.8177335742904],
+    [144.970386306518, -37.8173818877091],
+    [144.971989018237, -37.8169318797019]],
+  ]
+
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -36,42 +45,17 @@ export default function Map() {
         type: "geojson",
         data: {
           type: "FeatureCollection",
-          features: [
-            {
-              type: "Feature",
-              properties: {
-                color: "#F7455D", // red
-              },
-              geometry: {
-                type: "LineString",
-                coordinates: [
-                  [144.966964346166, -37.8183051340585],
-                  [144.967731536194, -37.8180840725787],
-                  [144.967915434574, -37.8180061406911],
-                  [144.968831710922, -37.8177335742904],
-                  [144.970386306518, -37.8173818877091],
-                  [144.971989018237, -37.8169318797019],
-                ],
-              },
+          features: coordinates.map(elem => ({
+            type: "Feature",
+            properties: {
+              color: "#F7455D", // red
             },
-            {
-              type: "Feature",
-              properties: {
-                color: "#33C9EB", // blue
-              },
-              geometry: {
-                type: "LineString",
-                coordinates: [
-                  [-37.8183051340585, 144.966964346166],
-                  [-37.8180840725787, 144.967731536194],
-                  [-37.8180061406911, 144.967915434574],
-                  [-37.8177335742904, 144.968831710922],
-                  [-37.8173818877091, 144.970386306518],
-                  [-37.8169318797019, 144.971989018237],
-                ],
-              },
+            geometry: {
+              type: "LineString",
+              coordinates: elem
             },
-          ],
+          })
+          )
         },
       });
       map.current.addLayer({
