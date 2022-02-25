7ed7e258efaa67aad870139392263f859629074d
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2022-02-25 21:30:55 +1100

    merge: conflits

--- mini-melbourne/src/Map/map.js
+++ mini-melbourne/src/Map/map.js
@@ -6,23 +6,23 @@ import stations from './data/stations.json';
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW5naWVneWkiLCJhIjoiY2t6eHo3bnZ3MDZkNTJvbnRhb3pucHNzMCJ9.M0LQFtd2bK54Z1UTDtzhAw';
 
+const coordinates = [
+  [
+    [144.966964346166, -37.8183051340585],
+    [144.967731536194, -37.8180840725787],
+    [144.967915434574, -37.8180061406911],
+    [144.968831710922, -37.8177335742904],
+    [144.970386306518, -37.8173818877091],
+    [144.971989018237, -37.8169318797019],
+  ],
+];
+
 export default function Map() {
   const [lng, setLng] = useState(144.966964346166);
   const [lat, setLat] = useState(-37.8183051340585);
   const [zoom, setZoom] = useState(10);
   const mapContainerRef = useRef(null);
   const [map, setMap] = useState(null);
-
-  const coordinates = [
-    [
-      [144.966964346166, -37.8183051340585],
-      [144.967731536194, -37.8180840725787],
-      [144.967915434574, -37.8180061406911],
-      [144.968831710922, -37.8177335742904],
-      [144.970386306518, -37.8173818877091],
-      [144.971989018237, -37.8169318797019],
-    ],
-  ];
 
   // Initialize map when component mounts
   useEffect(() => {
@@ -31,6 +31,12 @@ export default function Map() {
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom,
+    });
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
     });
 
     map.on('load', () => {
@@ -53,55 +59,42 @@ export default function Map() {
 
   useEffect(() => {
     if (map) {
-      map.on('move', () => {
-        setLng(map.getCenter().lng.toFixed(4));
-        setLat(map.getCenter().lat.toFixed(4));
-        setZoom(map.getZoom().toFixed(2));
-      });
-
-      map.on(
-        'load',
-        () => {
-          map.addSource('lines', {
-            type: 'geojson',
-            data: {
-              type: 'FeatureCollection',
-              features: coordinates.map((elem) => ({
-                type: 'Feature',
-                properties: {
-                  color: '#F7455D', // red
-                },
-                geometry: {
-                  type: 'LineString',
-                  coordinates: elem,
-                },
-              })),
+      map.addSource('lines', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: coordinates.map((elem) => ({
+            type: 'Feature',
+            properties: {
+              color: '#F7455D', // red
             },
-          });
-          map.addLayer({
-            id: 'lines',
-            type: 'line',
-            source: 'lines',
-            layout: {
-              'line-join': 'round',
-              'line-cap': 'round',
+            geometry: {
+              type: 'LineString',
+              coordinates: elem,
             },
-            paint: {
-              'line-width': 3,
-              // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
-              // to set the line-color to a feature property value.
-              'line-color': ['get', 'color'],
-            },
-          });
+          })),
         },
-        []
-      );
+      });
+      map.addLayer({
+        id: 'lines',
+        type: 'line',
+        source: 'lines',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-width': 3,
+          // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
+          // to set the line-color to a feature property value.
+          'line-color': ['get', 'color'],
+        },
+      });
     }
   }, [map]);
 
   return (
     <div>
-      <h1>Hello</h1>
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
