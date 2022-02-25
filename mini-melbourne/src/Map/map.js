263e1f377867f8d7157dbc25a4918a6654e7e27c
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2022-02-25 21:12:25 +1100

    feat: added markers

--- mini-melbourne/src/Map/map.js
+++ mini-melbourne/src/Map/map.js
@@ -1,42 +1,51 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import './map.css';
+import stations from './data/stations.json';
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW5naWVneWkiLCJhIjoiY2t6eHo3bnZ3MDZkNTJvbnRhb3pucHNzMCJ9.M0LQFtd2bK54Z1UTDtzhAw';
 
 export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
   const [lng, setLng] = useState(144.966964346166);
   const [lat, setLat] = useState(-37.8183051340585);
-  const [zoom, setZoom] = useState(12);
+  const [zoom, setZoom] = useState(10);
+  const mapContainerRef = useRef(null);
+  const [map, setMap] = useState(null);
 
+  // Initialize map when component mounts
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom,
     });
-  });
+
+    map.on('load', () => {
+      setMap(map);
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []);
 
   useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
+    if (map) {
+      stations.map((station) => {
+        new mapboxgl.Marker()
+          .setLngLat([station.stop_lon, station.stop_lat])
+          .addTo(map);
+      });
+    }
+  }, [map]);
 
   return (
     <div>
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-      <div ref={mapContainer} className="map-container" />
+      <div ref={mapContainerRef} className="map-container" />
     </div>
   );
 }
