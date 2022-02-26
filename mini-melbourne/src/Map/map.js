b5880b225a5f7dfe63c1c7d0fed7e0d3f1f7e922
angiegyi <angelagyi@hotmail.com>
2022-02-26 12:25:28 +1100

    added lines using deckgl

--- mini-melbourne/src/Map/map.js
+++ mini-melbourne/src/Map/map.js
@@ -1,21 +1,27 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Popup from '../Train/TrainData/Popup';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import './map.css';
-import stations from './data/stations.json';
+import React, { useRef, useEffect, useState } from "react";
+import Popup from "../Train/TrainData/Popup";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "./map.css";
+import stations from "./data/stations.json";
+import { MapboxLayer } from "@deck.gl/mapbox";
+import { PathLayer } from "@deck.gl/layers";
 
 mapboxgl.accessToken =
-  'pk.eyJ1IjoiYW5naWVneWkiLCJhIjoiY2t6eHo3bnZ3MDZkNTJvbnRhb3pucHNzMCJ9.M0LQFtd2bK54Z1UTDtzhAw';
+  "pk.eyJ1IjoiYW5naWVneWkiLCJhIjoiY2t6eHo3bnZ3MDZkNTJvbnRhb3pucHNzMCJ9.M0LQFtd2bK54Z1UTDtzhAw";
 
-const coordinates = [
-  [
-    [144.966964346166, -37.8183051340585],
-    [144.967731536194, -37.8180840725787],
-    [144.967915434574, -37.8180061406911],
-    [144.968831710922, -37.8177335742904],
-    [144.970386306518, -37.8173818877091],
-    [144.971989018237, -37.8169318797019],
-  ],
+const data = [
+  {
+    path: [
+      [144.966964346166, -37.8183051340585],
+      [144.967731536194, -37.8180840725787],
+      [144.967915434574, -37.8180061406911],
+      [144.968831710922, -37.8177335742904],
+      [144.970386306518, -37.8173818877091],
+      [144.971989018237, -37.8169318797019],
+    ],
+    name: "Richmond",
+    color: [255, 0, 0],
+  },
 ];
 
 export default function Map() {
@@ -29,21 +35,34 @@ export default function Map() {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom,
     });
 
-    map.on('move', () => {
+    map.on("move", () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
     });
 
-    map.on('load', () => {
-      setMap(map);
+    const deckLayer = new MapboxLayer({
+      id: "path-layer",
+      type: PathLayer,
+      data,
+      pickable: true,
+      widthScale: 20,
+      widthMinPixels: 2,
+      getPath: (d) => d.path,
+      getColor: (d) => d.color,
+      getWidth: (d) => 0.1,
     });
 
+    map.on("load", () => {
+      setMap(map);
+      map.addLayer(deckLayer);
+      // update the layer
+    });
     // Clean up on unmount
     return () => map.remove();
   }, []);
@@ -51,45 +70,9 @@ export default function Map() {
   useEffect(() => {
     if (map) {
       stations.map((station) => {
-        new mapboxgl.Marker()
+        return new mapboxgl.Marker()
           .setLngLat([station.stop_lon, station.stop_lat])
           .addTo(map);
-      });
-    }
-  }, [map]);
-
-  useEffect(() => {
-    if (map) {
-      map.addSource('lines', {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: coordinates.map((elem) => ({
-            type: 'Feature',
-            properties: {
-              color: '#F7455D', // red
-            },
-            geometry: {
-              type: 'LineString',
-              coordinates: elem,
-            },
-          })),
-        },
-      });
-      map.addLayer({
-        id: 'lines',
-        type: 'line',
-        source: 'lines',
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round',
-        },
-        paint: {
-          'line-width': 3,
-          // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
-          // to set the line-color to a feature property value.
-          'line-color': ['get', 'color'],
-        },
       });
     }
   }, [map]);
@@ -97,10 +80,10 @@ export default function Map() {
   return (
     <div>
       <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}  
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-      <div ref={mapContainer} className="map-container" />
-      <Popup nextStation="Flinders Street" etaTime="7:30pm" occupancy="Light"/>
+      <div ref={mapContainerRef} className="map-container" />
+      <Popup nextStation="Flinders Street" etaTime="7:30pm" occupancy="Light" />
     </div>
   );
 }
