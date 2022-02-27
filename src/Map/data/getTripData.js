bcd40942ecd7287dcf38fa425ad61b55083b3590
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2022-02-27 14:43:44 +1100

    feat: remove unused var

--- src/Map/data/getTripData.js
+++ src/Map/data/getTripData.js
@@ -1,34 +1,37 @@
-import getTrainLine from '../../Train/TrainData/GetTrainLine'
+import getTrainLine from '../../Train/TrainData/GetTrainLine';
 
 async function getData() {
-    const data = await fetch(`${process.env.REACT_APP_BACKEND_API_URL}/est_realtime`);
-    const json = await data.json();
-    return json;
+  const data = await fetch(
+    `${process.env.REACT_APP_BACKEND_API_URL}/est_realtime`
+  );
+  const json = await data.json();
+  return json;
+}
+
+export default async function getTripData() {
+  // get the trip ids from the path data
+  let liveData = await getData();
+  // console.log(liveData)
+  const tripIds = [];
+  const returnData = [];
+
+  for (let i = 0; i < liveData.services.length; i++) {
+    tripIds.push(liveData.services[i].trip_id);
   }
-  
-  export default async function getTripData() {
-      // get the trip ids from the path data
-      let liveData = await getData();
-      // console.log(liveData)
-      const tripIds = []
-      const returnData = []
-      
-      for (let i = 0; i < liveData.services.length; i ++) {
-          tripIds.push(liveData.services[i].trip_id);
-      }
-  
-      const data = await Promise.all(
-          tripIds.map(async (id) => {
-          const data = await fetch(`${process.env.REACT_APP_BACKEND_API_URL}/next_station/${id}`);
-          const trainNameData = await getTrainLine(id);
-          const json = await data.json();
-          returnData.push({
-            tripId: id, 
-            trainName: trainNameData.line_name,
-            data: json
-          });
-        })
-      ).then((data) => returnData);
-      return returnData;
-    }
-    
+
+  await Promise.all(
+    tripIds.map(async (id) => {
+      const data = await fetch(
+        `${process.env.REACT_APP_BACKEND_API_URL}/next_station/${id}`
+      );
+      const trainNameData = await getTrainLine(id);
+      const json = await data.json();
+      returnData.push({
+        tripId: id,
+        trainName: trainNameData.line_name,
+        data: json,
+      });
+    })
+  ).then((data) => returnData);
+  return returnData;
+}
